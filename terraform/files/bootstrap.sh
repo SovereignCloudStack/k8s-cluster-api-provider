accf7f3fa9f171f1c71a262b7bf6c25a520d1ca7
Malte Münch <muench@23technologies.cloud>
2021-06-29 09:52:59 +0200

    Bump cluster-api to 0.3.19 and pin components to 0.3.19
    Signed-off-by: Malte Münch <muench@23technologies.cloud>

--- terraform/files/bootstrap.sh
+++ terraform/files/bootstrap.sh
@@ -5,7 +5,7 @@
 
 # version
 VERSION_K9S="0.23.3"
-VERSION_CLUSTERCTL="0.3.16"
+VERSION_CLUSTERCTL="0.3.19"
 
 ## install tools and utils at local account
 
@@ -13,7 +13,7 @@ VERSION_CLUSTERCTL="0.3.16"
 sudo snap install kubectl --classic
 
 # install k9s
-echo "# install k9s ${VERSION_CLUSTERCTL}"
+echo "# install k9s ${VERSION_K9S}"
 curl -L https://github.com/derailed/k9s/releases/download/v${VERSION_K9S}/k9s_Linux_x86_64.tar.gz | tar zf - -x k9s
 sudo mv ./k9s /usr/local/bin/k9s
 
