dfb9453d7b5b72e884eec9a1f2104a45480e24c6
fynn.sp <spaeker@23technologies.cloud>
2021-07-29 19:43:05 +0200

    Fix Terraform Syntax
    Signed-off-by: fynn.sp <spaeker@23technologies.cloud>

--- terraform/mgmtcluster.tf
+++ terraform/mgmtcluster.tf
@@ -95,8 +95,8 @@ EOF
   }
 
   provisioner "file" {
-    content	= templatefile("files/template/upload_capi_image.sh.tmpl", { kubernetes_version = var.kubernetes_version, provider = var.cloud_provider})
-    destination	= "/home/${var.ssh_username}/upload_capi_image.sh"
+    content     = templatefile("files/template/upload_capi_image.sh.tmpl", { kubernetes_version = var.kubernetes_version, provider = var.cloud_provider })
+    destination = "/home/${var.ssh_username}/upload_capi_image.sh"
   }
 
   provisioner "file" {
@@ -122,7 +122,6 @@ EOF
     source      = "files/kubernetes-manifests.d/"
     destination = "/home/${var.ssh_username}"
   }
-
 
   provisioner "file" {
     content     = templatefile("files/template/clusterctl_template.sh", { cloud_provider = var.cloud_provider })
