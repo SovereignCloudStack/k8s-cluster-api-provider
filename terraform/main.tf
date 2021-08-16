b3aa917d26151b82f5a2f586ae3d6f5b18911bd4
Fynn Späker <spaeker@23technologies.cloud>
2021-08-16 15:11:07 +0200

    Upgrade version of the terraform openstack provider
    Signed-off-by: Fynn Späker <spaeker@23technologies.cloud>

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,7 +9,7 @@ terraform {
   required_providers {
     openstack = {
       source  = "terraform-provider-openstack/openstack"
-      version = "1.32.0"
+      version = "1.43.0"
     }
 
     local = {
