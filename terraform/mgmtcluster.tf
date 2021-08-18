01b0dc0e6c11f2a035a703f5419ffbee105f7a02
Kurt Garloff <scs@garloff.de>
2021-08-18 08:29:14 +0000

    Inject sysctl changing nf_conntrack_max to 131072.
    This addresses
    https://github.com/SovereignCloudStack/k8s-cluster-api-provider/issues/18
    https://github.com/kubernetes-sigs/kind/issues/2240
    
    Signed-off-by: Kurt Garloff <scs@garloff.de>

--- terraform/mgmtcluster.tf
+++ terraform/mgmtcluster.tf
@@ -55,6 +55,8 @@ runcmd:
   - mv /tmp/daemon.json /etc/docker/daemon.json
   - groupadd docker
   - usermod -aG docker ${var.ssh_username}
+  - echo net.netfilter.nf_conntrack_max=131072 > /etc/sysctl.d/90-conntrack_max.conf
+  - sysctl -w -p /etc/sysctl.d/90-conntrack_max.conf
   - apt -y install docker.io
 EOF
 
