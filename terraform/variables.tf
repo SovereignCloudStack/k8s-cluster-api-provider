3c737f471bc76ab34fd2030258992b3285a6eb5e
Kurt Garloff <kurt@garloff.de>
2021-08-25 00:52:59 +0200

    Upgrade default kubernetes version to 1.21.4. (#30)
    Signed-off-by: Kurt Garloff <scs@garloff.de>

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -36,7 +36,7 @@ variable "ssh_username" {
 variable "kubernetes_version" {
   description = "desired kubernetes version for the workload cluster"
   type        = string
-  default     = "v1.20.9"
+  default     = "v1.21.4"
 }
 
 variable "kind_mtu" {
