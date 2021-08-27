2bffcade2594557af4d60d7cf363f1c693cc9d70
Kurt Garloff <scs@garloff.de>
2021-08-27 03:36:15 +0000

    License identifier.
    Signed-off-by: Kurt Garloff <scs@garloff.de>

--- terraform/files/delete_cluster.sh
+++ terraform/files/delete_cluster.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # delete_cluster.sh [CLUSTERNAME]
+# (c) Kurt Garloff <garloff@osb-alliance.com>, 8/2021
+# SPDX-License-Identifier: Apache-2.0
 
 if test -n "$1"; then CLUSTER_NAME="$1"; else CLUSTER_NAME=testcluster; fi
 kubectl config use-context kind-kind
