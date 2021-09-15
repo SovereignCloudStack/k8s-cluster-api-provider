d46943eed59cb98a06e5bfa17d84674d941d6c4a
Kurt Garloff <kurt@garloff.de>
2021-09-15 06:02:07 +0000

    Pass SONOMODE opts to sonobuoy. Add quickcheck target.
    Signed-off-by: Kurt Garloff <kurt@garloff.de>

--- terraform/files/sonobuoy.sh
+++ terraform/files/sonobuoy.sh
@@ -5,6 +5,8 @@
 # (c) Kurt Garloff <garloff@osb-alliance.com>
 # SPDX-License-Identifier: Apache-2.0
 #
+unset TZ
+export LC_ALL=POSIX
 if ! test -x ./sonobuoy; then
 	SONOBUOY_VERSION=0.53.2
 	SONOTARBALL=sonobuoy_${SONOBUOY_VERSION}_linux_amd64.tar.gz
@@ -13,23 +15,26 @@ if ! test -x ./sonobuoy; then
 	chmod +x ./sonobuoy || exit 2
 	rm ${SONOTARBALL}
 fi
-echo "Running sonobuoy conformance tests ..."
+echo "=== Running sonobuoy conformance tests ... $@ ==="
 export KUBECONFIG=testcluster.yaml
-./sonobuoy run $1 || exit 3
-if test "$1" == "--light"; then SLP=10; else SLP=60; fi
+if ! test -r "$KUBECONFIG"; then echo "No $KUBECONFIG" 1>&2; exit 3; fi
+./sonobuoy run "$@" || exit 4
+if test "$1" == "--mode" -a "$2" == "quick"; then SLP=10; ALL=""; else SLP=60; ALL="--all"; fi
 while true; do
 	sleep $SLP
 	COMPLETE=$(./sonobuoy status)
+	date +%FT%TZ
 	echo "$COMPLETE"
 	if echo "$COMPLETE" | grep "has completed" >/dev/null 2>&1; then break; fi
 done
+echo "=== Collecting results ==="
 resfile=$(./sonobuoy retrieve)
 REPORT=$(./sonobuoy results $resfile)
 echo "$REPORT"
-./sonobuoy delete
+./sonobuoy delete $ALL
 declare -i fail=0
 while read number; do
 	let fail+=$number
 done < <(echo "$REPORT" | grep Failed | sed 's/Failed: //')
-if test $fail != 0; then exit i$((4+$fail)); fi
-echo "Sonobuoy conformance tests passed"
+if test $fail != 0; then exit $((4+$fail)); fi
+echo "=== Sonobuoy conformance tests passed ==="
