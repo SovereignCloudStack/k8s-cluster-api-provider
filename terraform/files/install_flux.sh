0122ec3bbe01f68266bf806aae0e432553163f0e
Kurt Garloff <kurt@garloff.de>
2022-02-04 08:54:50 +0000

    We don't need to run the whole install script for flux as root.
    Only the final mv needs root and gets it by itself.
    
    Signed-off-by: Kurt Garloff <kurt@garloff.de>

--- terraform/files/install_flux.sh
+++ terraform/files/install_flux.sh
@@ -3,7 +3,7 @@
 if test -x /usr/local/bin/flux; then exit 0; fi
 curl -s https://fluxcd.io/install.sh > install-flux2.sh
 chmod +x install-flux2.sh
-sudo ./install-flux2.sh
+./install-flux2.sh
 flux completion bash > ~/.bash_flux
 echo "source ~/.bash_flux" >> ~/.bash_aliases
 # flux install
