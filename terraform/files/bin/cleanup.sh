a6d09461558149eb052d0ffde3a0579afee8ba79
Kurt Garloff <kurt@garloff.de>
2022-03-04 17:49:09 +0000

    Call delete_cluster.sh with full path.
    Signed-off-by: Kurt Garloff <kurt@garloff.de>

--- terraform/files/bin/cleanup.sh
+++ terraform/files/bin/cleanup.sh
@@ -6,6 +6,6 @@ kubectl config use-context kind-kind
 CLUSTERS=$(kubectl get clusters | grep -v '^NAME' | awk '{ print $1; }')
 #for file in *-config.yaml; do cluster="${file%-config.yaml}"
 for cluster in $CLUSTERS; do
-	delete_cluster.sh "$cluster"
+	~/bin/delete_cluster.sh "$cluster"
 done
 kubectl get clusters
