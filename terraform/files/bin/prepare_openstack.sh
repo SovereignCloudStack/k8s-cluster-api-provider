21db86d265447f3abe621a2b2c94b174044f0530
Kurt Garloff <kurt@garloff.de>
2022-03-17 13:04:23 +0000

    Fix OS_CLOUD written to ~/.bash_aliases.
    Signed-off-by: Kurt Garloff <kurt@garloff.de>

--- terraform/files/bin/prepare_openstack.sh
+++ terraform/files/bin/prepare_openstack.sh
@@ -8,11 +8,11 @@ sudo apt install -y python3-openstackclient python3-octaviaclient
 sudo patch -p2 -N -d /usr/lib/python3/dist-packages/keystoneauth1 < /tmp/fix-keystoneauth-plugins-unversioned.diff
 
 # convenience
-echo "export OS_CLOUD=\"$PROVIDER\"" >> $HOME/.bash_aliases
+echo "export OS_CLOUD=\"$OS_CLOUD\"" >> $HOME/.bash_aliases
 
 # Determine project ID and inject into cloud.conf
 PROJECTID=$(openstack application credential show "${PREFIX}-appcred" -f value -c project_id)
-echo "Set tenant-id to $PROJECTID for $PROVIDER"
+echo "Set tenant-id to $PROJECTID for $OS_CLOUD"
 if ! grep '^tenant.id' ~/cluster-defaults/cloud.conf >/dev/null; then
   sed -i "/^application.credential.secret/atenant-id=$PROJECTID"  ~/cluster-defaults/cloud.conf
 fi
