af5fe6f1af8644c12df48bcfa2991ba0914fc307
Kurt Garloff <kurt@garloff.de>
2022-03-22 00:12:36 +0000

    Allow pinning the OCCM and cinderCSI versions.
    ANd we also have some logic to compute sane defaults.
    Needs doc & testing.
    
    Signed-off-by: Kurt Garloff <kurt@garloff.de>

--- terraform/files/bin/apply_openstack_integration.sh
+++ terraform/files/bin/apply_openstack_integration.sh
@@ -1,28 +1,54 @@
 #!/bin/bash
 export KUBECONFIG=~/.kube/config
 . ~/bin/cccfg.inc
+. ~/bin/openstack-kube-versions.inc
 
-echo "Install external OpenStack cloud provider to $CLUSTER_NAME"
 kubectl $KCONTEXT create secret generic cloud-config --from-file="$HOME/$CLUSTER_NAME/"cloud.conf -n kube-system #|| exit 6
 
 cd ~/kubernetes-manifests.d
 # install external cloud-provider openstack
-DEPLOY_K8S_OPENSTACK_GIT=$(yq eval '.DEPLOY_K8S_OPENSTACK_GIT' $CCCFG)
-if test "$DEPLOY_K8S_OPENSTACK_GIT" = "true"; then
-  for name in cloud-controller-manager-role-bindings.yaml cloud-controller-manager-roles.yaml openstack-cloud-controller-manager-ds.yaml openstack-cloud-controller-manager-pod.yaml; do
-    if ! test -s $name; then
-        curl -LO https://github.com/kubernetes/cloud-provider-openstack/raw/master/manifests/controller-manager/$name
-	echo -e "\n---" >> $name
+KUBERNETES_VERSION=$(yq eval '.KUBERNETES_VERSION' $CCCFG)
+DEPLOY_OCCM=$(yq eval '.DEPLOY_OCCM' $CCCFG)
+if test "$DEPLOY_OCCM" = "null"; then DEPLOY_OCCM=true;; fi
+if test "$DEPLOY_OCCM" = "false"; then exit 1; fi
+if test "$DEPLOY_OCCM" = "true"; then
+  find_openstack_versions $KUBERNETES_VERSION
+else
+  find_openstack_versions $DEPLOY_OCCM
+  if test "$OCCM_VERSION" = "$CCMR_VERSION"; then
+    OCCM_VERSION=$DEPLOY_OCCM
+    CCMR_VERSION=$DEPLOY_OCCM
+  else
+    OCCM_VERSION=$DEPLOY_OCCM
+  fi
+fi
+echo "Install external OpenStack cloud provider $OCCM_VERSION to $CLUSTER_NAME"
+
+if test -n "$OCCM_VERSION"; then
+  for name in openstack-cloud-controller-manager-ds.yaml openstack-cloud-controller-manager-pod.yaml; do
+    NAME=${name%.yaml}-$OCCM_VERSION.yaml
+    if test ! -s $NAME; then
+      curl -L https://github.com/kubernetes/cloud-provider-openstack/raw/$OCCM_VERSION/manifests/controller-manager/$name -o $NAME
+      echo -e "\n---" >> $NAME
     fi
   done
-  # Note: Do not deploy openstack-cloud-controller-manager-pod.yaml
-  cat cloud-controller-manager*.yaml > cloud-controller-manager-rbac-git.yaml
-  cat openstack-cloud-controller-manager-ds.yaml > openstack-git.yaml
-  CCMR=cloud-controller-manager-rbac-git.yaml
-  OCCM=openstack-git.yaml
+  OCCM=openstack-cloud-controller-manager-ds-$OCCM_VERSION.yaml
+else
+  OCCM=openstack.yaml
+fi
+
+if test -n "$CCMR_VERSION"; then
+  for name in cloud-controller-manager-role-bindings.yaml cloud-controller-manager-roles.yaml; do
+    NAME=${name%.yaml}-$CCMR_VERSION.yaml
+    if test ! -s $NAME; then
+      curl -L https://github.com/kubernetes/cloud-provider-openstack/raw/$CCMR_VERSION/manifests/controller-manager/$name -o $NAME
+      echo -e "\n---" >> $NAME
+    fi
+  done
+  cat cloud-controller-manager*-$CCMR_VERSION.yaml > cloud-controller-manager-rbac-$CCMR_VERSION.yaml
+  CCMR=cloud-controller-manager-rbac-$CCMR_VERSION.yaml
 else
   CCMR=cloud-controller-manager-rbac.yaml
-  OCCM=openstack.yaml
 fi
 if grep '\-\-cluster\-name=' $OCCM >/dev/null 2>&1; then
 	sed "/ *\- name: CLUSTER_NAME/n
