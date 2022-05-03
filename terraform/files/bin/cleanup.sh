3424c0bd518986faf5903791d200401b83140f98
Kurt Garloff <kurt@garloff.de>
2022-05-03 11:29:04 +0200

    Warn before deleting all clusters and allow ^C (#205)
    Signed-off-by: Kurt Garloff <kurt@garloff.de>

--- terraform/files/bin/cleanup.sh
+++ terraform/files/bin/cleanup.sh
@@ -4,6 +4,9 @@
 export KUBECONFIG=~/.kube/config
 kubectl config use-context kind-kind
 CLUSTERS=$(kubectl get clusters | grep -v '^NAME' | awk '{ print $1; }')
+echo "Deleting all clusters: $CLUSTERS"
+echo "Hit ^C to interrupt"
+sleep 3
 #for file in *-config.yaml; do cluster="${file%-config.yaml}"
 for cluster in $CLUSTERS; do
 	~/bin/delete_cluster.sh "$cluster"
