28c647314fb3d3f39b7a429cf8ed4461f597b640
Kurt Garloff <kurt@garloff.de>
2022-07-03 12:09:30 +0000

    Leave existing clusters alone. Robust clouds.yaml config generation.
    Signed-off-by: Kurt Garloff <kurt@garloff.de>

--- terraform/files/bin/create_appcred.sh
+++ terraform/files/bin/create_appcred.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
+# create_appcred.sh
+# Determine whether we need to create a per-cluster application credential
+# and add an appropriate config to the clouds.yaml section.
+# Call clusterctl_template.sh to update clusterctl.yaml
+# 
+# (c) Kurt Garloff <garloff@osb-alliance.com>, 7/2022
+# SPDX-License-Identifier: CC-BY-SA-4.0
+# 
 #Determine whether we need a new application credential
-unset APPCRED_ID APPCRED_PRJ
+export KUBECONFIG=$HOME/.kube/config
+kubectl config use-context kind-kind || exit 1
+# If the cluster exists already and we don't have a private appcred, leave it alone
+if kubectl get cluster $CLUSTER_NAME >/dev/null 2>&1 && ! grep '^OLD_OPENSTACK_CLOUD' ~/$CLUSTER_NAME/clusterctl.yaml; then
+  echo "#Warn: Old style cluster, disable new appcred handling"
+  exit 0
+fi
 APPCREDS=$(openstack application credential list -f value -c ID -c Name -c "Project ID")
 while read id nm prjid; do
 	#echo "\"$nm\" \"$prjid\" \"$id\""
@@ -20,10 +34,27 @@ if test -z "$APPCRED_ID"; then
 	read APPCRED_ID APPCRED_PRJ APPCRED_SECRET < <(echo $NEWCRED)
 	echo "#Created AppCred $APPCRED_ID"
 	if test ! -e ~/.config/openstack/clouds.yaml.orig; then cp -p ~/.config/openstack/clouds.yaml ~/.config/openstack/clouds.yaml.orig; fi
-	# FIXME: Generate a fresh section rather than relying on cleanliness
-	print-cloud.py -c $PREFIX-$CLUSTER_NAME -r application_credential_id=$APPCRED_ID -r application_credential_secret="\"$APPCRED_SECRET\"" -i auth_url="#project_id: $APPCRED_PRJ" | grep -v '^#' | grep -v '^---' | grep -v '^clouds:' >> ~/.config/openstack/clouds.yaml
-	# And remove from env
-	unset APPCRED_SECRET
+	#print-cloud.py -c $PREFIX-$CLUSTER_NAME -r application_credential_id=$APPCRED_ID -r application_credential_secret="\"$APPCRED_SECRET\"" -i auth_url="#project_id: $APPCRED_PRJ" | grep -v '^#' | grep -v '^---' | grep -v '^clouds:' >> ~/.config/openstack/clouds.yaml
+	# Generate a fresh section rather than relying on cleanliness of existing setup
+	AUTH_URL=$(print-cloud.py | yq eval .clouds.${OS_CLOUD}.auth.auth_url -)
+	REGION=$(print-cloud.py | yq eval .clouds.${OS_CLOUD}.region_name -)
+	# In theory we could also make interface and id_api_vers variable,
+	# but let's do that once we find the necessity. Error handling makes
+	# it slightly complex, so it's not an obvious win.
+	cat >> ~/.config/openstack/clouds.yaml <<EOT
+  $PREFIX-$CLUSTER_NAME:
+    interface: public
+    identity_api_version: 3
+    region_name: $REGION
+    auth_type: "v3applicationcredential"
+    auth:
+      auth_url: $AUTH_URL
+      #project_id: $APPCRED_PRJ
+      application_credential_id: $APPCRED_ID
+      application_credential_secret: "$APPCRED_SECRET"
+EOT
+	# And remove secret from env
+	unset APPCRED_SECRET NEWCRED
 fi
 export OS_CLOUD=$PREFIX-$CLUSTER_NAME
 export PROJECTID=$APPCRED_PRJ
