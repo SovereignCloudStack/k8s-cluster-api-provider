40b03aadd0e82d951d1e161f69e2a1d92e66ea51
Kurt Garloff <kurt@garloff.de>
2022-08-10 17:55:33 +0000

    Add ca to produce a -fullchain style PEM.
    Also:
    - Avoid asking again when having signed already.
    - Test jq and cfssl tools availability.
    - Usage insns as comment.
    - Error out when tools report failure.
    
    Signed-off-by: Kurt Garloff <kurt@garloff.de>

--- terraform/files/bin/signer.sh
+++ terraform/files/bin/signer.sh
@@ -2,6 +2,14 @@
 # k8s signer
 # (c) Kurt Garloff <garloff@osb-alliance.com>, 8/2022
 # SPDX-License-Identifier: CC-BY-SA-4.0
+#
+# Usage:
+# * Create a directory where you put the kubernetes ca.crt and .key.
+# * Go into this directory
+# * Calling signer.sh CLUSTERNAME will sign all CSRs that have been marked approved
+#   with the CA cert and push the cert back into k8s.
+# * For Pending requests, it will display and interactively ask, which can be
+#   changed using options -f and -a.
 
 usage()
 {
@@ -25,6 +33,9 @@ if test ! -r ca.crt -o ! -r ca.key; then
 	exit 2
 fi
 
+if ! type -p jq >/dev/null; then echo "Need jq installed"; exit 2; fi
+if ! type -p cfssl >/dev/null; then echo "Need cfssl installed"; exit 2; fi
+
 if test ! -r server-signing-config.json; then cat > server-signing-config.json <<EOT
 {
     "signing": {
@@ -46,19 +57,23 @@ fi
 
 sign()
 {
+	# FIXME: Need to determine API version
 	CERTAPI=v1beta1
-	REQ=$(kubectl $CTX get csr $1 -o jsonpath='{.spec.request}' | base64 --decode)
+	REQ=$(kubectl $CTX get csr $1 -o jsonpath='{.spec.request}' | base64 --decode) || exit 4
 	OSSLINFO=$(echo "$REQ" | openssl req -noout -text -in -)
 	echo "Signing $1: $OSSLINFO"
-	SIGNED=$(echo "$REQ" | cfssl sign -ca ca.crt -ca-key ca.key -config server-signing-config.json - | cfssljson -bare signed-$1)
+	echo "$REQ" | cfssl sign -ca ca.crt -ca-key ca.key -config server-signing-config.json - | cfssljson -bare signed-$1
+	if test "${PIPESTATUS[1]}" != "0"; then exit 5; fi
+	# Append issuer cert to have full trust chain
+	cat ca.crt >> signed-$1.pem
 	kubectl $CTX get csr $1 -o json | jq '.status.certificate = "'$(base64 signed-$1.pem | tr -d '\n')'"' | \
-		kubectl $CTX replace --raw /apis/certificates.k8s.io/$CERTAPI/certificatesigningrequests/$1/status -f -
+		kubectl $CTX replace --raw /apis/certificates.k8s.io/$CERTAPI/certificatesigningrequests/$1/status -f - || exit 6
 }
 
 
 while read req age signer requestor status; do
 	if test "$status" = "Approved,Issued"; then continue
-	elif test "$status" = "Approved"; then sign $req
+	elif test "$status" = "Approved"; then sign $req; continue
 	elif test "$status" != "Pending"; then continue
 	fi
 	#FIXME: Should ask openstack whether DNS nama and IP match ....
