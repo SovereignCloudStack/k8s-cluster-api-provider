d42b51e33875a697f878052e0d6521aeaf4aa1fa
Christian Berendt <berendt@23technologies.cloud>
2022-08-16 13:34:15 +0200

    Use double quotes for $UBU_IMG_NM when using openstack image list
    Closes #235
    
    Signed-off-by: Christian Berendt <berendt@23technologies.cloud>

--- terraform/files/bin/wait_capi_image.sh
+++ terraform/files/bin/wait_capi_image.sh
@@ -9,7 +9,7 @@ UBU_IMG_NM=$(yq eval '.OPENSTACK_IMAGE_NAME' $CCCFG)
 echo -n "Waiting for image $UBU_IMG_NM to become active: "
 let -i ctr=0
 while test $ctr -lt 180; do
-  CAPIIMG=$(openstack image list --name $UBU_IMG_NM -f value -c ID -c Status)
+  CAPIIMG=$(openstack image list --name "$UBU_IMG_NM" -f value -c ID -c Status)
   if test -z "$CAPIIMG"; then
     echo "Image $UBU_IMG_NM does not exist, create ..."
     $HOME/bin/upload_capi_image.sh "$1" || exit $?
