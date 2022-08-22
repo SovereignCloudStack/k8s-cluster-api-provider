28240a716e52c39bf999367e2cbddf66498a72ad
Kurt Garloff <kurt@garloff.de>
2022-08-22 18:50:47 +0200

    Use PREFIX for all OpenStack resources.
    This means we should be able to create two management clusters (with
    distinct prefixes) in the same OpenStack project without causing
    naming conflicts.
    
    Signed-off-by: Kurt Garloff <kurt@garloff.de>

--- terraform/files/bin/enable-cilium-sg.sh
+++ terraform/files/bin/enable-cilium-sg.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 . ~/bin/cccfg.inc
-SGS=$(openstack security group list -f value -c ID -c Name | grep "k8s-cluster-${CLUSTER_NAME}-cilium")
+SGS=$(openstack security group list -f value -c ID -c Name | grep "${PREFIX}-${CLUSTER_NAME}-cilium")
 if test -z "$SGS"; then
-    SGS=$(openstack security group create k8s-cluster-${CLUSTER_NAME}-cilium -f value -c id -c name)
+    SGS=$(openstack security group create ${PREFIX}-${CLUSTER_NAME}-cilium -f value -c id -c name)
     # Note: Silium connectivity test will requires tcp/3xxxx/EchoOther tcp/3xxxx/EchoSame
     # See ports with k get svc -A
     # Should this really be required?
@@ -15,7 +15,7 @@ if test -z "$SGS"; then
 	port=${port%/*}
 	if test "${port%:*}" == "$port"; then port="$port:$port"; fi
 	# Note: we could instead use --remote-ip ${NODE_CIDR} -- less secure, but better performance
-	openstack security group rule create --description "capi $CLUSTER_NAME $desc" --ingress --ethertype IPv4 --proto $prot --dst-port $port --remote-group $SG $SG
+	openstack security group rule create --description "$PREFIX $CLUSTER_NAME $desc" --ingress --ethertype IPv4 --proto $prot --dst-port $port --remote-group $SG $SG
     done
 fi
 # SD is consumed in cluster-template.yaml
