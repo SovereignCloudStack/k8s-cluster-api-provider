93b30316970cf8e374db9b4bc89eb3e172ed035b
Fynn Späker <spaeker@23technologies.cloud>
2022-09-07 12:28:16 +0200

    Add possibility to choose git repo
    Signed-off-by: Fynn Späker <spaeker@23technologies.cloud>

--- terraform/mgmtcluster.tf
+++ terraform/mgmtcluster.tf
@@ -156,7 +156,7 @@ EOF
   # FIXME: We should get the branch (and warnings for unpushed changes from the Makefile)
   provisioner "remote-exec" {
     inline = [
-      "/tmp/get_k8s_git.sh ${var.git_branch}",
+      "/tmp/get_k8s_git.sh ${var.git_repo} ${var.git_branch}",
       "/home/${var.ssh_username}/bin/bootstrap.sh"
     ]
   }
