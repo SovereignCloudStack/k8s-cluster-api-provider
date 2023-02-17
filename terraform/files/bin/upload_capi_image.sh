d38034a6effeeb2d1db4c0df429198ffd83b2c63
Kurt Garloff <kurt@garloff.de>
2023-02-17 18:25:56 +0000

    Use Ubuntu 22.04 images for the latest k8s versions.
    This uses 22.04 starting from 1.25.6 and 1.26[.0].
    Also: Move the k8s version parsing in a separate scriptlet to be sourced.
    
    Signed-off-by: Kurt Garloff <kurt@garloff.de>

--- terraform/files/bin/upload_capi_image.sh
+++ terraform/files/bin/upload_capi_image.sh
@@ -4,19 +4,29 @@
 
 ~/bin/fixup_k8s_version.sh $CCCFG
 KUBERNETES_VERSION=$(yq eval '.KUBERNETES_VERSION' $CCCFG)
+. ~/bin/parse_k8s_version.inc
 #UBU_IMG_NM=ubuntu-capi-image-$KUBERNETES_VERSION
 UBU_IMG_NM=$(yq eval '.OPENSTACK_IMAGE_NAME' $CCCFG)
 IMG_RAW=$(yq eval '.OPENSTACK_IMAGE_RAW' $CCCFG)
 IMGREG_EXTRA=$(yq eval '.OPENSTACK_IMAGE_REGISTRATION_EXTRA_FLAGS' $CCCFG)
 
 VERSION_CAPI_IMAGE=$(echo $KUBERNETES_VERSION | sed 's/\.[[:digit:]]*$//g')
-UBU_IMG=ubuntu-2004-kube-$KUBERNETES_VERSION
+if test $K8SVER -ge 12600 ||
+   test $K8SVER -lt 12600 -a $K8SVER -ge 12506; then
+  UBUVER=2204
+  UBUVERS="22.04"
+else
+  UBUVER=2004
+  UBUVERS="20.04"
+fi
+
+UBU_IMG=ubuntu-$UBUVER-kube-$KUBERNETES_VERSION
 
 WAITLOOP=64
 #download/upload image to openstack
 CAPIIMG=$(openstack image list --name "$UBU_IMG_NM")
 IMGURL=https://minio.services.osism.tech/openstack-k8s-capi-images
-IMAGESRC=$IMGURL/ubuntu-2004-kube-$VERSION_CAPI_IMAGE/$UBU_IMG.qcow2
+IMAGESRC=$IMGURL/ubuntu-$UBUVER-kube-$VERSION_CAPI_IMAGE/$UBU_IMG.qcow2
 if test -z "$CAPIIMG"; then
   # TODO: Check signature
   wget $IMAGESRC
@@ -37,7 +47,7 @@ if test -z "$CAPIIMG"; then
   #TODO min-disk, min-ram, other std. image metadata
   mkdir -p ~/tmp
   echo "Creating image $UBU_IMG_NM from $UBU_IMG.$FMT"
-  nohup openstack image create --disk-format $FMT --min-ram 1024 --min-disk $DISKSZ --property image_build_date="$IMGDATE" --property image_original_user=ubuntu --property architecture=x86_64 --property hypervisor_type=kvm --property os_distro=ubuntu --property os_version="20.04" --property hw_disk_bus=scsi --property hw_scsi_model=virtio-scsi --property hw_rng_model=virtio --property image_source=$IMAGESRC --property image_description="https://github.com/osism/k8s-capi-images" --property kubernetes_version=$KUBERNETES_VERSION --property replace_frequency=never --property provided_until=$UNTIL --property uuid_validity=$UNTIL --tag managed_by_osism $IMGREG_EXTRA --file $UBU_IMG.$FMT $UBU_IMG_NM  > ~/tmp/img-create-$UBU_IMG_NM.out &
+  nohup openstack image create --disk-format $FMT --min-ram 1024 --min-disk $DISKSZ --property image_build_date="$IMGDATE" --property image_original_user=ubuntu --property architecture=x86_64 --property hypervisor_type=kvm --property os_distro=ubuntu --property os_version="$UBUVERS" --property hw_disk_bus=scsi --property hw_scsi_model=virtio-scsi --property hw_rng_model=virtio --property image_source=$IMAGESRC --property image_description="https://github.com/osism/k8s-capi-images" --property kubernetes_version=$KUBERNETES_VERSION --property replace_frequency=never --property provided_until=$UNTIL --property uuid_validity=$UNTIL --tag managed_by_osism $IMGREG_EXTRA --file $UBU_IMG.$FMT $UBU_IMG_NM  > ~/tmp/img-create-$UBU_IMG_NM.out &
   CPID=$!
   sleep 5
   echo "Waiting for image $UBU_IMG_NM: "
