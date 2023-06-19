bc9580e7469ff986dcb939a7dd4e4f44763d3fcc
Kurt Garloff <kurt@garloff.de>
2023-06-19 18:10:22 +0200

    Update CAPI to v1.3.8 and CAPO to 0.7.3. (#427)
    * Update CAPI to v1.3.8 and CAPO to 0.7.3.
    
    These are bugfix only changes.
    CAPI-v1.3.8:
        clusterctl: Return error on infra cluster and control plane discovery (#8610)
        e2e: Adjust machinepool helper e2e timeout (#8755)
        e2e: test/e2e check for machines being ready after provisioning on Runtime SDK test (#8647)
        e2e: test/framework fix docker pod log collector (#8644)
        KCP: Allow machine rollout if cert reconcile fails (#8738)
        KCP: Prevent KCP to create many private keys for each reconcile (#8626)
        CAPD: change the haproxy entrypoint to prevent getting stopped immediately after start (#8741)
        CAPD: Delegate CAPD port selection to the container runtime (#8654)
    CAPI-v1.3.7:
        clusterctl: Use local kustomize version in create-local-repository.py (#8436)
        Dependency: Update kindnetd and kindest/haproxy (#8471)
    CAPI-v1.3.6:
        API: Implements Getter interface for IPAddressClaim object (#8380)
        test: Make it possible to run envtest-based integration tests from vscode (#8212)
    
    CAPO-v0.7.3:
        Always filter cluster subnets by cluster network ID (#1573)
    CAPO-v0.7.2:
        Backport Octavia OVN provider support to release-0.7 (#1529)
        Infer port network from subnet (#1526)
    
    * Update documentation and default-environment.
    
    Thanks for spotting this, @chess-knight!
    
    Signed-off-by: Kurt Garloff <kurt@garloff.de>

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -58,13 +58,13 @@ variable "calico_version" {
 variable "clusterapi_version" {
   description = "desired version of cluster-api"
   type        = string
-  default     = "1.3.5"
+  default     = "1.3.8"
 }
 
 variable "capi_openstack_version" {
   description = "desired version of the OpenStack cluster-api provider"
   type        = string
-  default     = "0.7.1"
+  default     = "0.7.3"
 }
 
 variable "kubernetes_version" {
