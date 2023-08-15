ca796fefe38e1d0b101cc4f47e93b5ef8f4e54fc
Filip Dobrovolný <dobrovolny.filip@gmail.com>
2023-08-15 13:18:28 +0300

    Fix delete_cluster.sh and others to handle per cluster ns on os #492 (#494)
    Openstack resource names are generated by CAPI based on the NS the cluster
    is in. This fix takes that in account for scripts on the management node.
    
    Signed-off-by: Filip Dobrovolny <dobrovolny.filip@gmail.com>

--- terraform/files/bin/remove_cluster-network.sh
+++ terraform/files/bin/remove_cluster-network.sh
@@ -3,16 +3,28 @@ export KUBECONFIG=~/.kube/config
 . ~/.capi-settings
 . ~/bin/cccfg.inc
 
+NET_NAME=$(openstack network list -f value -c Name | grep "k8s-clusterapi-cluster-\(default-${CLUSTER_NAME}\|${CLUSTER_NAME}-${CLUSTER_NAME}\)")
+
 # Determine mgmtserver networks
 MGMT="$PREFIX-mgmtcluster"
 MGMTNET=$(openstack server list --name "$MGMT" -f value -c Networks)
-NET=$(echo "$MGMTNET" | grep "k8s-clusterapi-cluster-default-$CLUSTER_NAME=" | sed "s/.*k8s-clusterapi-cluster-default-$CLUSTER_NAME=\([0-9a-f:\.]*\).*\$/\1/")
-# New format
-if test -z "$NET"; then NET=$(echo "$MGMTNET" | grep "'k8s-clusterapi-cluster-default-$CLUSTER_NAME':" | sed "s/.*'k8s-clusterapi-cluster-default-$CLUSTER_NAME':[^']*'\([0-9a-f:\.]*\)'.*\$/\1/"); fi
-if test -z "$NET"; then echo "No network to remove ..."; exit 1; fi
+if [[ $NET_NAME == "k8s-clusterapi-cluster-default-"* ]]; then
+  # Old format of network name based on cluster in default namespace
+  NET=$(echo "$MGMTNET" | grep "k8s-clusterapi-cluster-default-$CLUSTER_NAME=" | sed "s/.*k8s-clusterapi-cluster-default-$CLUSTER_NAME=\([0-9a-f:\.]*\).*\$/\1/")
+  # New format
+  if test -z "$NET"; then NET=$(echo "$MGMTNET" | grep "'k8s-clusterapi-cluster-default-$CLUSTER_NAME':" | sed "s/.*'k8s-clusterapi-cluster-default-$CLUSTER_NAME':[^']*'\([0-9a-f:\.]*\)'.*\$/\1/"); fi
+else
+  # New format of network name based on cluster in cluster with namespace name as cluster name
+  NET=$(echo "$MGMTNET" | grep "k8s-clusterapi-cluster-$CLUSTER_NAME-$CLUSTER_NAME=" | sed "s/.*k8s-clusterapi-cluster-$CLUSTER_NAME-$CLUSTER_NAME=\([0-9a-f:\.]*\).*\$/\1/")
+  # New format
+  if test -z "$NET"; then NET=$(echo "$MGMTNET" | grep "'k8s-clusterapi-cluster-$CLUSTER_NAME-$CLUSTER_NAME':" | sed "s/.*'k8s-clusterapi-cluster-$CLUSTER_NAME-$CLUSTER_NAME':[^']*'\([0-9a-f:\.]*\)'.*\$/\1/"); fi
+fi
+if test -z "$NET"; then
+  echo "No network to remove ..."
+  exit 1
+fi
 NIC=$(ip addr | grep -B4 "inet $NET/" | grep '^[0-9]' | sed 's/^[0-9]*: \([^: ]*\): .*$/\1/')
 
 #sudo ip link set dev ens8 down
 echo "Removing NIC $NIC $NET ..."
-openstack server remove network $MGMT k8s-clusterapi-cluster-default-$CLUSTER_NAME || exit
-
+openstack server remove network $MGMT $NET_NAME || exit
