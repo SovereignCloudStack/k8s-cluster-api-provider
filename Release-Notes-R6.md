c15678a214c1607dd295186f9f78f4b1bf851774
Filip Dobrovolny <dobrovolny.filip@gmail.com>
2023-10-20 10:11:30 +0200

    Address code review changes
    Signed-off-by: Filip Dobrovolny <dobrovolny.filip@gmail.com>

--- Release-Notes-R6.md
+++ Release-Notes-R6.md
@@ -4,3 +4,5 @@
 
 - [#617](https://github.com/SovereignCloudStack/k8s-cluster-api-provider/pull/617)
   Install kubectx on management node
+- [#618](https://github.com/SovereignCloudStack/k8s-cluster-api-provider/pull/618)
+  Install kube_ps1 on management node
--- terraform/files/bin/install_kube_ps1.sh
+++ terraform/files/bin/install_kube_ps1.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-# install kube-ps1
-git clone https://github.com/jonmosco/kube-ps1 ~/.kube-ps1
+KUBEPS1_VERSION=v0.8.0
+echo "# install kube-ps1 $KUBEPS1_VERSION"
+git clone --depth 1 --branch $KUBEPS1_VERSION https://github.com/jonmosco/kube-ps1 ~/.kube-ps1
