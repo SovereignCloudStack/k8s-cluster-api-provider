d1577ee2b519e02130631861756b21871fcddad3
Filip Dobrovolny <dobrovolny.filip@gmail.com>
2023-10-20 09:59:14 +0200

    Address code review changes to pin kubectx version
    Signed-off-by: Filip Dobrovolny <dobrovolny.filip@gmail.com>

--- terraform/files/bin/install_kubectx.sh
+++ terraform/files/bin/install_kubectx.sh
@@ -2,8 +2,10 @@
 
 sudo apt-get install --no-install-recommends --no-install-suggests -y fzf
 
+KUBECTX_VERSION=v0.9.5
+echo "# install kubectx $KUBECTX_VERSION"
 # Deploy kubectx on mgmt cluster for convenience
-git clone https://github.com/ahmetb/kubectx.git ~/.kubectx
+git clone --depth 1 --branch $KUBECTX_VERSION https://github.com/ahmetb/kubectx.git ~/.kubectx
 COMPDIR=$(pkg-config --variable=completionsdir bash-completion)
 ln -sf ~/.kubectx/completion/kubens.bash $COMPDIR/kubens
 ln -sf ~/.kubectx/completion/kubectx.bash $COMPDIR/kubectx
