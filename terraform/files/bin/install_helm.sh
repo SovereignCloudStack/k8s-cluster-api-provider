7a4209dc4c7b384a9f4870d3ea886f52ccaeebc9
Malte Münch <muench@b1-systems.de>
2023-10-31 22:02:43 +0100

    source proxy settings in files/bin/install_helm.sh (curl)
    Signed-off-by: Malte Münch <muench@b1-systems.de>

--- terraform/files/bin/install_helm.sh
+++ terraform/files/bin/install_helm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Download and deploy helm
-
+. /etc/profile.d/proxy.sh
 HELMVER=3.12.3
 OS=linux; ARCH=$(uname -m | sed 's/x86_64/amd64/')
 curl -LO https://get.helm.sh/helm-v${HELMVER}-$OS-$ARCH.tar.gz
