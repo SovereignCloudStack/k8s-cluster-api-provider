f821eb362068f23fe29f03c684349ff7261b777b
Malte Münch <muench@b1-systems.de>
2023-11-08 22:02:45 +0100

    Adds script to configure containerd to use proxy on worker and controlplane
    Signed-off-by: Malte Münch <muench@b1-systems.de>

--- terraform/files/bin/configure_containerd_proxy.sh
+++ terraform/files/bin/configure_containerd_proxy.sh
@@ -14,7 +14,7 @@
 #     [Service]
 #     Environment="HTTP_PROXY=<$HTTP_PROXY from /etc/profile.d/proxy.sh>"
 #     Environment="HTTPS_PROXY=<$HTTP_PROXY from /etc/profile.d/proxy.sh>"
-#     Environment="NO_PROXY=<my-no-proxy-configuration>"
+#     Environment="NO_PROXY=127.0.0.0/8,172.18.0.0/16,fc00:f853:ccd:e793::/64,10.96.0.0/16,10.244.0.0/16,kind-control-plane,.svc,.svc.cluster,.svc.cluster.local"
 #   ```
 # - Injects temporary YAML file into $1 (cluster-template.yaml) file (using `yq` in place edit).
 #   Temporary file is injected to the `KubeadmControlPlane.spec.kubeadmConfigSpec.files` that specifies extra files to be
@@ -24,12 +24,49 @@
 #
 # (c) Malte Muench, 11/2023
 # SPDX-License-Identifier: Apache-2.0
+if test -z "$1"; then echo "ERROR: Need cluster-template.yaml arg" 1>&2; exit 1; fi
 
 . /etc/profile.d/proxy.sh
 
-if [ -v $HTTP_PROXY ]
+if [ ! -v HTTP_PROXY ]
 then
-echo "I am going to set $HTTP_PROXY as the proxy server for containerd"
+echo "No HTTP_PROXY set, nothing to do, exiting."
+exit 0
 fi
 
+export SYSTEMD_CONFIG_CONTENT=containerd_systemd_conf
+export CLUSTER_TEMPLATE_SNIPPET=clustertemplate_snippet
+
+echo "[Service]" > $SYSTEMD_CONFIG_CONTENT
+echo "Environment=\"HTTP_PROXY=$HTTP_PROXY\"" >> $SYSTEMD_CONFIG_CONTENT
+echo "Environment=\"HTTPS_PROXY=$HTTP_PROXY\"" >> $SYSTEMD_CONFIG_CONTENT
+echo "Environment=\"NO_PROXY=127.0.0.0/8,172.18.0.0/16,fc00:f853:ccd:e793::/64,10.96.0.0/16,10.244.0.0/16,kind-control-plane,.svc,.svc.cluster,.svc.cluster.local\"" >> $SYSTEMD_CONFIG_CONTENT
+
+
+yq --null-input '
+  .path = "/etc/systemd/system/containerd.service.d/http-proxy.conf" |
+  .owner = "root:root" |
+  .permissions = "0644" |
+  .content = loadstr(env(SYSTEMD_CONFIG_CONTENT))' > $CLUSTER_TEMPLATE_SNIPPET
+
+# Test whether the file is already present in cluster-template.yaml
+file_cp_exist=$(yq 'select(.kind == "KubeadmControlPlane").spec.kubeadmConfigSpec | (.files // (.files = []))[] | select(.path == "/etc/systemd/system/containerd.service.d/http-proxy.conf")' "$1")
+
+if test -z "$file_cp_exist"; then
+	echo "Adding containerd proxy config to the KubeadmControlPlane files"
+	yq 'select(.kind == "KubeadmControlPlane").spec.kubeadmConfigSpec.files += [load(env(CLUSTER_TEMPLATE_SNIPPET))]' -i "$1"
+else
+        echo "containerd proxy config is already defined in KubeadmControlPlane files"
+fi
+
+file_ct_exist=$(yq 'select(.kind == "KubeadmConfigTemplate").spec.template.spec | (.files // (.files = []))[] | select(.path == "/etc/systemd/system/containerd.service.d/http-proxy.conf")' "$1")
+if test -z "$file_ct_exist"; then
+    echo "Adding containerd proxy config to the KubeadmConfigTemplate files"
+    yq 'select(.kind == "KubeadmConfigTemplate").spec.template.spec.files += [load(env(CLUSTER_TEMPLATE_SNIPPET))]' -i "$1"
+else
+    echo "containerd proxy config is already defined in KubeadmConfigTemplate files"
+fi
+
+rm $SYSTEMD_CONFIG_CONTENT
+rm $CLUSTER_TEMPLATE_SNIPPET
 exit 0
