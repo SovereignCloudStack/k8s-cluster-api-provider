2fb1f0c21efbdc4c1bcef0928246466c4c58b716
Malte Münch <muench@b1-systems.de>
2023-11-15 16:20:21 +0100

    Adds script that copies proxy settings from mgmt host to cluster-template.yaml
    Signed-off-by: Malte Münch <muench@b1-systems.de>

---
+++ terraform/files/bin/configure_proxy.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+# ./configure_containerd_proxy.sh cluster-template.yaml
+#
+# Script injects proxy profile config  file into $1 (cluster-template.yaml).
+# Script reads proxy configuration from /etc/profile.d/proxy.sh
+#
+# - Creates temporary YAML file with the proxy config as follows:
+#   ```yaml
+#   ---
+#   path: /etc/profile.d/proxy.sh
+#   owner: "root:root"
+#   permissions: "0644"
+#   content: |
+#   <content form /etc/profile.d/proxy.sh>
+#   ```
+# - Injects temporary YAML file into $1 (cluster-template.yaml) file (using `yq` in place edit).
+#   Temporary file is injected to the `KubeadmControlPlaneTemplate.spec.kubeadmConfigSpec.files` that specifies extra files to be
+#   passed to user_data upon creation of control plane nodes and to the `KubeadmConfigTemplate.spec.template.spec.files`
+#   that specifies extra files to be passed to user_data upon creation of worker nodes.
+# - Removes temporary YAML file
+#
+# (c) Malte Muench, 11/2023
+# SPDX-License-Identifier: Apache-2.0
+if test -z "$1"; then echo "ERROR: Need cluster-template.yaml arg" 1>&2; exit 1; fi
+
+export PROFILE_CONFIG_CONTENT=proxy-profile.sh
+export CLUSTER_TEMPLATE_SNIPPET=clustertemplate_snippet
+
+# yq might be installed as snap which can not read /etc
+cp /etc/profile.d/proxy.sh $PROFILE_CONFIG_CONTENT
+
+
+yq --null-input '
+  .path = "/etc/profile.d/proxy.sh" |
+  .owner = "root:root" |
+  .permissions = "0644" |
+  .content = loadstr(env(PROFILE_CONFIG_CONTENT))' > $CLUSTER_TEMPLATE_SNIPPET
+
+# Test whether the file is already present in cluster-template.yaml
+file_cp_exist=$(yq 'select(.kind == "KubeadmControlPlaneTemplate").spec.template.spec.kubeadmConfigSpec | (.files // (.files = []))[] | select(.path == "/etc/profile.d/proxy.sh")' "$1")
+
+if test -z "$file_cp_exist"; then
+	echo "Adding containerd proxy config to the KubeadmControlPlaneTemplate files"
+	yq 'select(.kind == "KubeadmControlPlaneTemplate").spec.template.spec.kubeadmConfigSpec.files += [load(env(CLUSTER_TEMPLATE_SNIPPET))]' -i "$1"
+else
+        echo "proxy profile config is already defined in KubeadmControlPlaneTemplate files"
+fi
+
+file_ct_exist=$(yq 'select(.kind == "KubeadmConfigTemplate").spec.template.spec | (.files // (.files = []))[] | select(.path == "/etc/profile.d/proxy.sh")' "$1")
+if test -z "$file_ct_exist"; then
+    echo "Adding containerd proxy config to the KubeadmConfigTemplate files"
+    yq 'select(.kind == "KubeadmConfigTemplate").spec.template.spec.files += [load(env(CLUSTER_TEMPLATE_SNIPPET))]' -i "$1"
+else
+    echo "proxy profile config is already defined in KubeadmConfigTemplate files"
+fi
+
+rm $PROFILE_CONFIG_CONTENT
+rm $CLUSTER_TEMPLATE_SNIPPET
+exit 0
