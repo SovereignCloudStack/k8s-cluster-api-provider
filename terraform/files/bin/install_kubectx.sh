83286f27ef6a1710652529402d11a930a7827c13
Paul Hildebrandt <hildebrandt@b1-systems.de>
2023-11-19 23:52:56 +0100

    Fixes pkg-config error in kubectx installation
    Signed-off-by: Paul Hildebrandt <hildebrandt@b1-systems.de>

--- terraform/files/bin/install_kubectx.sh
+++ terraform/files/bin/install_kubectx.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-sudo apt-get install --no-install-recommends --no-install-suggests -y fzf
+sudo apt-get install --no-install-recommends --no-install-suggests -y fzf pkg-config
 
 KUBECTX_VERSION=v0.9.5
 echo "# install kubectx $KUBECTX_VERSION"
 # Deploy kubectx on mgmt cluster for convenience
 git clone --depth 1 --branch $KUBECTX_VERSION https://github.com/ahmetb/kubectx.git ~/.kubectx
 COMPDIR=$(pkg-config --variable=completionsdir bash-completion)
-ln -sf ~/.kubectx/completion/kubens.bash $COMPDIR/kubens
-ln -sf ~/.kubectx/completion/kubectx.bash $COMPDIR/kubectx
+sudo ln -sf ~/.kubectx/completion/kubens.bash $COMPDIR/kubens
+sudo ln -sf ~/.kubectx/completion/kubectx.bash $COMPDIR/kubectx
 cat << EOF >> ~/.bashrc
 
 #kubectx and kubens
