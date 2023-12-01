1fa2d598f0586598b6904cbc323ed491ac7863e3
Roman Hros <roman.hros@dnation.cloud>
2023-12-01 09:51:42 +0100

    Fix metrics server upgrade
    I find it during the upgrade process of the SCS container registry cluster SovereignCloudStack/k8s-harbor#54
    
    Signed-off-by: Roman Hros <roman.hros@dnation.cloud>

--- terraform/files/bin/apply_metrics.sh
+++ terraform/files/bin/apply_metrics.sh
@@ -15,8 +15,8 @@ METRICS_VERSION=v0.6.4
 
 echo "Deploy metrics server to $CLUSTER_NAME"
 # Metrics server
-if test ! -s ~/kubernetes-manifests.d/metrics-server.yaml; then
-	curl -L https://github.com/kubernetes-sigs/metrics-server/releases/download/$METRICS_VERSION/components.yaml | sed '/        - --kubelet-use-node-status-port/a\        - --kubelet-insecure-tls' > ~/kubernetes-manifests.d/metrics-server.yaml
+if test ! -s ~/kubernetes-manifests.d/metrics-server-${METRICS_VERSION}.yaml; then
+	curl -L https://github.com/kubernetes-sigs/metrics-server/releases/download/$METRICS_VERSION/components.yaml | sed '/        - --kubelet-use-node-status-port/a\        - --kubelet-insecure-tls' > ~/kubernetes-manifests.d/metrics-server-${METRICS_VERSION}.yaml
 fi
-cp -p ~/kubernetes-manifests.d/metrics-server.yaml ~/${CLUSTER_NAME}/deployed-manifests.d/
+cp -p ~/kubernetes-manifests.d/metrics-server-${METRICS_VERSION}.yaml ~/${CLUSTER_NAME}/deployed-manifests.d/metrics-server.yaml
 kubectl --context=$KCONTEXT apply -f ~/${CLUSTER_NAME}/deployed-manifests.d/metrics-server.yaml || exit 9
